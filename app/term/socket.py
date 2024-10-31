414c17d2d345f9280c8afb8fbd3d3b91faf6e7b2
proin <proin@season.co.kr>
2024-10-31 21:18:53 +0900

    2.4.8

--- app/term/socket.py
+++ app/term/socket.py
@@ -7,66 +7,108 @@ import struct
 import fcntl
 import shlex
 import select
+import psutil
 
-def set_winsize(fd, row, col, xpix=0, ypix=0):
+def set_winsize(fd, row, col, xpix=9, ypix=17):
     winsize = struct.pack("HHHH", row, col, xpix, ypix)
     fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
+
+def getCache(wiz, namespace):
+    if 'wiz_terminal' not in wiz.server.app: 
+        wiz.server.app.wiz_terminal = season.util.stdClass()
+    cache = wiz.server.app.wiz_terminal
+    if namespace not in cache:
+        cache[namespace] = season.util.stdClass()
+    return cache[namespace]
 
 class Controller:
     def __init__(self, server):
         self.server = server
 
     def connect(self, wiz):
-        try: season.util.fn.call(wiz.server.config.ide.acl, wiz=wiz)
+        pass
+
+    def close(self, wiz, data, io):
+        try: wiz.server.config.ide.acl(wiz)
         except: return
 
-        app = wiz.server.app.flask
-        socketio = wiz.server.app.socketio
+        namespace = data['namespace']
+        cache = getCache(wiz, namespace)
+        if cache["child_pid"] is not None:
+            ppid = cache["child_pid"]
+            parent = psutil.Process(ppid)
+            for child in parent.children(recursive=True):
+                pid = child.pid
+                os.system(f"kill -9 {pid} > /dev/null")
+            os.system(f"kill -9 {ppid} > /dev/null")
 
+    def join(self, wiz, data, io):
+        try: wiz.server.config.ide.acl(wiz)
+        except: return
+        to = data['namespace']
+        io.join(to)
+
+    def create(self, wiz, data, io):
+        try: wiz.server.config.ide.acl(wiz)
+        except: return
+        cache_namespace = data['namespace']
+        cache = getCache(wiz, cache_namespace)
+        namespace = "/wiz/ide/app/xterm.app.term"
+        to = f"{cache_namespace}"
+        socketio = wiz.server.app.socketio
+        
         def read_and_forward_pty_output():
             max_read_bytes = 1024 * 20
             while True:
                 socketio.sleep(0.01)
                 try:
-                    if app.config["fd"]:
+                    if cache["fd"]:
                         timeout_sec = 0
-                        (data_ready, _, _) = select.select([app.config["fd"]], [], [], timeout_sec)
+                        (data_ready, _, _) = select.select([cache["fd"]], [], [], timeout_sec)
                         if data_ready:
-                            output = os.read(app.config["fd"], max_read_bytes).decode(errors="ignore")
-                            socketio.emit("ptyoutput", {"output": output}, namespace="/wiz/ide/app/xterm.app.term")
+                            output = os.read(cache["fd"], max_read_bytes).decode(errors="ignore")
+                            if "'unknown': I need something more specific." in output:
+                                output = output.replace("'unknown': I need something more specific.", "\033\143")
+                            io.emit("ptyoutput", {"output": output}, to=to, namespace=namespace)
                 except Exception as e:
-                    app.config["child_pid"] = None
-                    app.config["fd"] = None
+                    io.emit("exit", {}, to=to, namespace=namespace)
+                    cache["fd"] = None
+                    break
 
-        if "child_pid" not in app.config: app.config["child_pid"] = None
-        if "fd" not in app.config: app.config["fd"] = None
-        app.config["cmd"] = "bash"
+        if "child_pid" not in cache: 
+            cache["child_pid"] = None
+        if "fd" not in cache: 
+            cache["fd"] = None
 
-        if app.config["child_pid"]:
+        cache["cmd"] = f"bash".split(" ")
+        if cache["child_pid"]:
             return
 
         (child_pid, fd) = pty.fork()
+
         if child_pid == 0:
-            subprocess.run(app.config["cmd"])
+            os.environ['TERM'] = 'xterm-256color'
+            subprocess.run(cache["cmd"])
         else:
-            app.config["fd"] = fd
-            app.config["child_pid"] = child_pid
-            set_winsize(fd, 50, 50)
-            cmd = " ".join(shlex.quote(c) for c in app.config["cmd"])
+            cache["fd"] = fd
+            cache["child_pid"] = child_pid
+            set_winsize(fd, data["rows"], data["cols"])
+            cmd = " ".join(shlex.quote(c) for c in cache["cmd"])
             socketio.start_background_task(target=read_and_forward_pty_output)
-
+        
     def ptyinput(self, wiz, data):
-        try: season.util.fn.call(wiz.server.config.ide.acl, wiz=wiz)
+        try: wiz.server.config.ide.acl(wiz)
         except: return
-
-        app = wiz.server.app.flask
-        if app.config["fd"]:
-            os.write(app.config["fd"], data["input"].encode())
+        namespace = data['namespace']
+        cache = getCache(wiz, namespace)
+        if cache["fd"]:
+            os.write(cache["fd"], data["input"].encode())
 
     def resize(self, wiz, data):
-        try: season.util.fn.call(wiz.server.config.ide.acl, wiz=wiz)
+        try: wiz.server.config.ide.acl(wiz)
         except: return
-
-        app = wiz.server.app.flask
-        if app.config["fd"]:
-            set_winsize(app.config["fd"], data["rows"], data["cols"])
+            
+        namespace = data['namespace']
+        cache = getCache(wiz, namespace)
+        if cache["fd"]:
+            set_winsize(cache["fd"], data["rows"], data["cols"])
--- app/term/view.ts
+++ app/term/view.ts
@@ -1,21 +1,43 @@
-import { OnInit, ElementRef, ViewChild } from '@angular/core';
+import { OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/core';
 import { Service } from '@wiz/service/service';
 import { io } from "socket.io-client";
-import { Terminal } from 'xterm';
-import { FitAddon } from 'xterm-addon-fit';
-import { SearchAddon } from 'xterm-addon-search';
-import { WebLinksAddon } from 'xterm-addon-web-links';
+import { Terminal } from '@xterm/xterm';
+import { FitAddon } from '@xterm/addon-fit';
+import { SearchAddon } from '@xterm/addon-search';
+import { WebLinksAddon } from '@xterm/addon-web-links';
 
-export class Component implements OnInit {
+export class Component implements OnInit, OnDestroy {
     @ViewChild('terminal')
     public terminal: ElementRef;
+
+    public socket: any;
+    public term: any;
+    public fit: any;
+    public namespace: any;
 
     public APP_ID: string = wiz.namespace;
 
     constructor(public service: Service) { }
 
+    public async socketEmit(channel: string, data: any = {}) {
+        data.namespace = this.namespace;
+        this.socket.emit(channel, data);
+    }
+
     public async ngOnInit() {
         await this.service.render();
+
+        const generateRandomString = (num) => {
+            const characters = 'abcdefghijklmnopqrstuvwxyz';
+            let result = '';
+            const charactersLength = characters.length;
+            for (let i = 0; i < num; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+        }
+
+        this.namespace = generateRandomString(8);
 
         const modw = 9;
         const modh = 17;
@@ -35,28 +57,40 @@ export class Component implements OnInit {
         term.resize(cols, rows);
 
         term.open(this.terminal.nativeElement);
-        term.writeln("Welcome to wiz.term!");
-        term.writeln('')
+        term.writeln("Welcome to wiz terminal!");
+        term.writeln('');
+
+        const socket = io.connect("/wiz/ide/app/xterm.app.term");;
+
         term.onData((data) => {
-            socket.emit("ptyinput", { input: data });
+            this.socketEmit("ptyinput", { input: data })
         });
 
-        const socket = io.connect("/wiz/ide/app/xterm.app.term");
-        socket.on("ptyoutput", function (data) {
+        socket.on("ptyoutput", async (data) => {
             term.write(data.output);
         });
 
         socket.on("connect", () => {
             fitToscreen();
+            const dims = { cols: term.cols, rows: term.rows };
+            this.socketEmit("join");
+            this.socketEmit("create", dims);
         });
 
         socket.on("disconnect", () => {
         });
 
-        function fitToscreen() {
+        let fitToscreen = () => {
             fit.fit();
             const dims = { cols: term.cols, rows: term.rows };
-            socket.emit("resize", dims);
+            this.socketEmit("resize", dims);
         }
+
+        this.socket = socket;
+    }
+
+    public async ngOnDestroy() {
+        this.socketEmit('close');
+        this.socket.close();
     }
 }
